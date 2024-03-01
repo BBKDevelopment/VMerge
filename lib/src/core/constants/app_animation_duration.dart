adf11adee89d5e85468073b4ff566a44f3093760
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2024-03-01 21:55:44 +0100

    refactor: update animation durations

--- lib/src/core/constants/app_animation_duration.dart
+++ lib/src/core/constants/app_animation_duration.dart
@@ -1,10 +1,10 @@
 /// A class that provides constant animation duration values to use app-wide.
 abstract final class AppAnimationDuration {
   /// [short] duration equals to `400 milliseconds`.
-  static const short = Duration(milliseconds: 400);
+  static const short = Duration(milliseconds: 600);
 
   /// [medium] duration equals to `600 milliseconds`.
-  static const medium = Duration(milliseconds: 600);
+  static const medium = Duration(milliseconds: 800);
 
   /// [long] duration equals to `1000 milliseconds`.
   static const long = Duration(milliseconds: 1000);
