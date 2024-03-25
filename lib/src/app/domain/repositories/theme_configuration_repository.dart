256ac1da7d098092c0776fd4a21c94ab102578e7
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2024-03-25 17:26:58 +0100

    feat: create ThemeConfigurationRepository and its implementation in app

---
+++ lib/src/app/domain/repositories/theme_configuration_repository.dart
@@ -0,0 +1,12 @@
+import 'package:vmerge/src/app/app.dart';
+import 'package:vmerge/src/core/core.dart';
+
+abstract interface class ThemeConfigurationRepository {
+  const ThemeConfigurationRepository();
+
+  Future<DataState<ThemeConfiguration>> getThemeConfiguration();
+
+  Future<DataState<bool>> saveThemeConfiguration(
+    ThemeConfiguration configuration,
+  );
+}
