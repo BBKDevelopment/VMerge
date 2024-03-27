7bc1ba509666a82db2bdad3fbe582773b8fbaf60
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2024-03-27 18:12:02 +0100

    feat: update ThemeBottomSheet theme to use material you guidelines

--- lib/src/features/more/presentation/widgets/theme_bottom_sheet.dart
+++ lib/src/features/more/presentation/widgets/theme_bottom_sheet.dart
@@ -5,58 +5,51 @@ class _ThemeBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(color: context.theme.dividerColor),
-        ),
-      ),
-      child: Padding(
-        padding: AppPadding.general,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(width: AppButtonSize.small),
-                Hero(
-                  tag: MorePageOption.theme.name,
-                  child: Assets.images.palette.svg(
-                    height: AppIconSize.xLarge,
+    return Padding(
+      padding: AppPadding.general,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(width: AppButtonSize.small),
+              Hero(
+                tag: MorePageOption.theme.name,
+                child: Assets.images.palette.svg(
+                  height: AppIconSize.xLarge,
+                  colorFilter: ColorFilter.mode(
+                    context.theme.iconTheme.color!,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+              SizedBox.square(
+                dimension: AppButtonSize.small,
+                child: IconButton.filledTonal(
+                  onPressed: Navigator.of(context).pop,
+                  icon: Assets.images.close.svg(
+                    height: AppIconSize.xxSmall,
                     colorFilter: ColorFilter.mode(
-                      context.theme.iconTheme.color!,
+                      context.colorScheme.onSecondaryContainer,
                       BlendMode.srcIn,
                     ),
                   ),
                 ),
-                SizedBox.square(
-                  dimension: AppButtonSize.small,
-                  child: IconButton.filledTonal(
-                    onPressed: Navigator.of(context).pop,
-                    icon: Assets.images.close.svg(
-                      height: AppIconSize.xxSmall,
-                      colorFilter: ColorFilter.mode(
-                        context.colorScheme.onSecondaryContainer,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: AppPadding.medium),
-            Text(
-              context.l10n.theme,
-              style: context.textTheme.titleLarge,
-            ),
-            const SizedBox(height: AppPadding.xxLarge),
-            const _ModeSelector(),
-            const Divider(height: AppPadding.large),
-            const _MainColorSelector(),
-          ],
-        ),
+              ),
+            ],
+          ),
+          const SizedBox(height: AppPadding.medium),
+          Text(
+            context.l10n.theme,
+            style: context.textTheme.titleLarge,
+          ),
+          const SizedBox(height: AppPadding.xxLarge),
+          const _ModeSelector(),
+          const Divider(height: AppPadding.large),
+          const _MainColorSelector(),
+        ],
       ),
     );
   }
