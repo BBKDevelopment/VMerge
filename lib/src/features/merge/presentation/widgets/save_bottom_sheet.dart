761327c4ae98ed644833b74b8f130bb223b215cc
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2024-04-15 22:39:49 +0200

    refactor: create sub-widgets for _SaveBottomSheet widget

--- lib/src/features/merge/presentation/widgets/save_bottom_sheet.dart
+++ lib/src/features/merge/presentation/widgets/save_bottom_sheet.dart
@@ -27,158 +27,236 @@ class _SaveBottomSheetState extends State<_SaveBottomSheet> {
 
     return Padding(
       padding: AppPadding.allLarge,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(width: AppButtonSize.small),
+              Hero(
+                tag: 'save',
+                child: Assets.images.save.svg(
+                  height: AppIconSize.xLarge,
+                  colorFilter: ColorFilter.mode(
+                    context.theme.iconTheme.color!,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+              SizedBox.square(
+                dimension: AppButtonSize.small,
+                child: IconButton.filledTonal(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: Assets.images.close.svg(
+                    height: AppIconSize.xxSmall,
+                    colorFilter: ColorFilter.mode(
+                      context.colorScheme.onSecondaryContainer,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: AppPadding.medium),
+          Text(
+            context.l10n.saveVideo,
+            style: context.textTheme.titleLarge,
+          ),
+          const SizedBox(height: AppPadding.xxLarge),
+          const _ProgressIndicator(),
+          const SizedBox(height: AppPadding.large),
+          const _Status(),
+          const _StatusMessage(),
+          const _GalleryButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ProgressIndicator extends StatelessWidget {
+  const _ProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      width: 100,
       child: BlocBuilder<SaveBottomSheetCubit, SaveBottomSheetState>(
         builder: (context, state) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
+          return Stack(
+            fit: StackFit.expand,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(width: AppButtonSize.small),
-                  Hero(
-                    tag: 'save',
-                    child: Assets.images.save.svg(
-                      height: AppIconSize.xLarge,
-                      colorFilter: ColorFilter.mode(
-                        context.theme.iconTheme.color!,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
-                  SizedBox.square(
-                    dimension: AppButtonSize.small,
-                    child: IconButton.filledTonal(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: Assets.images.close.svg(
-                        height: AppIconSize.xxSmall,
-                        colorFilter: ColorFilter.mode(
-                          context.colorScheme.onSecondaryContainer,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: AppPadding.medium),
-              Text(
-                context.l10n.saveVideo,
-                style: context.textTheme.titleLarge,
-              ),
-              const SizedBox(height: AppPadding.xxLarge),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    CircularProgressIndicator(
-                      backgroundColor:
-                          context.theme.iconTheme.color?.withOpacity(0.7),
-                      strokeWidth: 8,
-                      value: state.progress / 100,
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        switch (state.status) {
-                          SaveBottomSheetStatus.error =>
-                            context.colorScheme.error,
-                          _ => context.theme.iconTheme.color!,
-                        },
-                      ),
-                    ),
-                    Center(
-                      child: switch (state.status) {
-                        SaveBottomSheetStatus.success => Icon(
-                            Icons.check_rounded,
-                            color: context.theme.iconTheme.color,
-                            size: AppIconSize.large,
-                          ),
-                        SaveBottomSheetStatus.error => Icon(
-                            Icons.close_rounded,
-                            color: context.colorScheme.error,
-                            size: AppIconSize.large,
-                          ),
-                        _ => Text(
-                            '${state.progress}%',
-                            textAlign: TextAlign.center,
-                            style: context.textTheme.titleMedium,
-                          ),
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: AppPadding.large),
-              AnimatedSwitcher(
-                duration: AppAnimationDuration.medium,
-                transitionBuilder: (child, animation) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: SlideTransition(
-                      position: Tween(
-                        begin: const Offset(0, 0.3),
-                        end: Offset.zero,
-                      ).animate(animation),
-                      child: child,
-                    ),
-                  );
-                },
-                child: Text(
+              CircularProgressIndicator(
+                backgroundColor:
+                    context.theme.iconTheme.color?.withOpacity(0.7),
+                strokeWidth: 8,
+                value: state.progress / 100,
+                valueColor: AlwaysStoppedAnimation<Color>(
                   switch (state.status) {
-                    SaveBottomSheetStatus.analyse => l10n.analyzing,
-                    SaveBottomSheetStatus.merge => l10n.merging,
-                    SaveBottomSheetStatus.save => l10n.saving,
-                    SaveBottomSheetStatus.success => l10n.done,
-                    SaveBottomSheetStatus.error => l10n.error,
+                    SaveBottomSheetStatus.error => context.colorScheme.error,
+                    _ => context.theme.iconTheme.color!,
                   },
-                  key: ValueKey('title:${state.status.name}'),
-                  style: context.textTheme.titleMedium,
                 ),
               ),
-              Container(
-                height: 80,
-                padding: AppPadding.horizontalLarge,
-                child: AnimatedSwitcher(
-                  duration: AppAnimationDuration.short,
-                  child: Text(
-                    switch (state.status) {
-                      SaveBottomSheetStatus.analyse => l10n.analyzingMessage,
-                      SaveBottomSheetStatus.merge => l10n.mergingMessage,
-                      SaveBottomSheetStatus.save => l10n.savingMessage,
-                      SaveBottomSheetStatus.success => l10n.doneMessage,
-                      SaveBottomSheetStatus.error => l10n.errorMessage,
-                    },
-                    key: ValueKey('subtitle:${state.status.name}'),
-                    style: context.textTheme.bodyMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              AnimatedContainer(
-                duration: AppAnimationDuration.short,
-                height: switch (state.status) {
-                  SaveBottomSheetStatus.success => kMinInteractiveDimension,
-                  _ => 0.0,
+              Center(
+                child: switch (state.status) {
+                  SaveBottomSheetStatus.success => Icon(
+                      Icons.check_rounded,
+                      color: context.theme.iconTheme.color,
+                      size: AppIconSize.large,
+                    ),
+                  SaveBottomSheetStatus.error => Icon(
+                      Icons.close_rounded,
+                      color: context.colorScheme.error,
+                      size: AppIconSize.large,
+                    ),
+                  _ => Text(
+                      '${state.progress}%',
+                      textAlign: TextAlign.center,
+                      style: context.textTheme.titleMedium,
+                    ),
                 },
-                child: AnimatedOpacity(
-                  opacity: switch (state.status) {
-                    SaveBottomSheetStatus.success => 1.0,
-                    _ => 0.0,
-                  },
-                  duration: AppAnimationDuration.long,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(l10n.seeInTheGallery),
-                  ),
-                ),
               ),
             ],
           );
         },
       ),
+    );
+  }
+}
+
+class _Status extends StatelessWidget {
+  const _Status();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return BlocSelector<SaveBottomSheetCubit, SaveBottomSheetState,
+        SaveBottomSheetStatus>(
+      selector: (state) => state.status,
+      builder: (context, status) {
+        return AnimatedSwitcher(
+          duration: AppAnimationDuration.medium,
+          transitionBuilder: (child, animation) {
+            return FadeTransition(
+              opacity: animation,
+              child: SlideTransition(
+                position: Tween(
+                  begin: const Offset(0, 0.3),
+                  end: Offset.zero,
+                ).animate(animation),
+                child: child,
+              ),
+            );
+          },
+          child: Text(
+            switch (status) {
+              SaveBottomSheetStatus.analyse => l10n.analyzing,
+              SaveBottomSheetStatus.merge => l10n.merging,
+              SaveBottomSheetStatus.save => l10n.saving,
+              SaveBottomSheetStatus.success => l10n.done,
+              SaveBottomSheetStatus.error => l10n.error,
+            },
+            key: ValueKey('$_Status:${status.name}'),
+            style: context.textTheme.titleMedium,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _StatusMessage extends StatelessWidget {
+  const _StatusMessage();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Container(
+      height: 80,
+      padding: AppPadding.horizontalLarge,
+      child: BlocSelector<SaveBottomSheetCubit, SaveBottomSheetState,
+          SaveBottomSheetStatus>(
+        selector: (state) => state.status,
+        builder: (context, status) {
+          return AnimatedSwitcher(
+            duration: AppAnimationDuration.short,
+            child: Text(
+              switch (status) {
+                SaveBottomSheetStatus.analyse => l10n.analyzingMessage,
+                SaveBottomSheetStatus.merge => l10n.mergingMessage,
+                SaveBottomSheetStatus.save => l10n.savingMessage,
+                SaveBottomSheetStatus.success => l10n.doneMessage,
+                SaveBottomSheetStatus.error => l10n.errorMessage,
+              },
+              key: ValueKey('$_StatusMessage:${status.name}'),
+              style: context.textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _GalleryButton extends StatelessWidget {
+  const _GalleryButton();
+
+  static const galleryIntent = AndroidIntent(
+    action: 'android.intent.action.MAIN',
+    category: 'android.intent.category.APP_GALLERY',
+    flags: [Flag.FLAG_ACTIVITY_NEW_TASK],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return BlocSelector<SaveBottomSheetCubit, SaveBottomSheetState,
+        SaveBottomSheetStatus>(
+      selector: (state) => state.status,
+      builder: (context, status) {
+        return AnimatedContainer(
+          duration: AppAnimationDuration.short,
+          height: switch (status) {
+            SaveBottomSheetStatus.success => kMinInteractiveDimension,
+            _ => 0.0,
+          },
+          child: AnimatedOpacity(
+            opacity: switch (status) {
+              SaveBottomSheetStatus.success => 1.0,
+              _ => 0.0,
+            },
+            duration: AppAnimationDuration.long,
+            child: TextButton(
+              onPressed: () async {
+                if (Platform.isAndroid) {
+                  try {
+                    await galleryIntent.launch();
+                  } catch (error, stackTrace) {
+                    log(
+                      'Failed to launch gallery!',
+                      name: '$_SaveBottomSheet',
+                      error: error,
+                      stackTrace: stackTrace,
+                    );
+                  }
+                }
+              },
+              child: Text(l10n.seeInTheGallery),
+            ),
+          ),
+        );
+      },
     );
   }
 }
