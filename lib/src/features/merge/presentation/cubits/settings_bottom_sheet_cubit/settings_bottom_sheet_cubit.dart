1c7be6274635fddca531a9808a7d5d26ee856662
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2024-05-07 17:48:28 +0200

    feat: create more states based on SettingsBottomSheetState

--- lib/src/features/merge/presentation/cubits/settings_bottom_sheet_cubit/settings_bottom_sheet_cubit.dart
+++ lib/src/features/merge/presentation/cubits/settings_bottom_sheet_cubit/settings_bottom_sheet_cubit.dart
@@ -11,7 +11,7 @@ final class SettingsBottomSheetCubit extends Cubit<SettingsBottomSheetState> {
   })  : _getMergeSettingsUseCase = getMergeSettingsUseCase,
         _saveMergeSettingsUseCase = saveMergeSettingsUseCase,
         super(
-          SettingsBottomSheetState(
+          SettingsBottomSheetLoaded(
             isSoundOn: _defaultMergeSettings.isSoundOn,
             playbackSpeed: _defaultMergeSettings.playbackSpeed,
             videoResolution: _defaultMergeSettings.videoResolution,
@@ -33,7 +33,7 @@ final class SettingsBottomSheetCubit extends Cubit<SettingsBottomSheetState> {
     final settings = await _getMergeSettings();
 
     emit(
-      state.copyWith(
+      SettingsBottomSheetLoaded(
         isSoundOn: settings.isSoundOn,
         playbackSpeed: settings.playbackSpeed,
         videoResolution: settings.videoResolution,
@@ -46,14 +46,14 @@ final class SettingsBottomSheetCubit extends Cubit<SettingsBottomSheetState> {
     final dataState = await _getMergeSettingsUseCase();
 
     switch (dataState) {
-      case final DataSuccess<MergeSettings> success:
-        return success.data;
-      case final DataFailure<MergeSettings> failure:
+      case DataSuccess():
+        return dataState.data;
+      case DataFailure():
         log(
-          failure.message,
-          name: failure.name,
-          error: failure.error,
-          stackTrace: failure.stackTrace,
+          dataState.message,
+          name: dataState.name,
+          error: dataState.error,
+          stackTrace: dataState.stackTrace,
         );
         await _saveMergeSettings(_defaultMergeSettings);
         return _defaultMergeSettings;
@@ -66,58 +66,76 @@ final class SettingsBottomSheetCubit extends Cubit<SettingsBottomSheetState> {
     switch (dataState) {
       case DataSuccess():
         break;
-      case final DataFailure<void> failure:
+      case DataFailure():
         log(
-          failure.message,
-          name: failure.name,
-          error: failure.error,
-          stackTrace: failure.stackTrace,
+          dataState.message,
+          name: dataState.name,
+          error: dataState.error,
+          stackTrace: dataState.stackTrace,
         );
+        final recoveryState = state;
+        emit(
+          SettingsBottomSheetError(
+            errorType: SettingsBottomSheetErrorType.saveSettingsException,
+            error: dataState.error,
+            stackTrace: dataState.stackTrace,
+          ),
+        );
+        emit(recoveryState);
     }
   }
 
   void _saveStateAsMergeSettings() {
-    final settings = MergeSettings(
-      isSoundOn: state.isSoundOn,
-      playbackSpeed: state.playbackSpeed,
-      videoResolution: state.videoResolution,
-      videoAspectRatio: state.videoAspectRatio,
-    );
-
-    _saveMergeSettings(settings);
+    if (state case final SettingsBottomSheetLoaded loadedState) {
+      final settings = MergeSettings(
+        isSoundOn: loadedState.isSoundOn,
+        playbackSpeed: loadedState.playbackSpeed,
+        videoResolution: loadedState.videoResolution,
+        videoAspectRatio: loadedState.videoAspectRatio,
+      );
+      _saveMergeSettings(settings);
+    }
   }
 
   void toggleSound({required bool isSoundOn}) {
-    emit(state.copyWith(isSoundOn: isSoundOn));
-    _saveStateAsMergeSettings();
+    if (state case final SettingsBottomSheetLoaded loadedState) {
+      emit(loadedState.copyWith(isSoundOn: isSoundOn));
+      _saveStateAsMergeSettings();
+    }
   }
 
   void changePlaybackSpeed(PlaybackSpeed playbackSpeed) {
-    // Slider gets triggered multiple times when the user drags it. This check
-    // prevents the settings from being saved multiple times.
-    if (state.playbackSpeed == playbackSpeed) return;
+    if (state case final SettingsBottomSheetLoaded loadedState) {
+      // Slider gets triggered multiple times when the user drags it. This
+      // check prevents the settings from being saved multiple times.
+      if (loadedState.playbackSpeed == playbackSpeed) return;
 
-    emit(state.copyWith(playbackSpeed: playbackSpeed));
-    _saveStateAsMergeSettings();
+      emit(loadedState.copyWith(playbackSpeed: playbackSpeed));
+      _saveStateAsMergeSettings();
+    }
   }
 
   void changeVideoResolution(VideoResolution videoResolution) {
-    final newState = switch (videoResolution) {
-      VideoResolution.original => state.copyWith(
-          videoResolution: videoResolution,
-          videoAspectRatio: VideoAspectRatio.firstVideo,
-        ),
-      _ => state.copyWith(
-          videoResolution: videoResolution,
-          videoAspectRatio: VideoAspectRatio.auto,
-        ),
-    };
-    emit(newState);
-    _saveStateAsMergeSettings();
+    if (state case final SettingsBottomSheetLoaded loadedState) {
+      final newState = switch (videoResolution) {
+        VideoResolution.original => loadedState.copyWith(
+            videoResolution: videoResolution,
+            videoAspectRatio: VideoAspectRatio.firstVideo,
+          ),
+        _ => loadedState.copyWith(
+            videoResolution: videoResolution,
+            videoAspectRatio: VideoAspectRatio.auto,
+          ),
+      };
+      emit(newState);
+      _saveStateAsMergeSettings();
+    }
   }
 
   void changeVideoAspectRatio(VideoAspectRatio videoAspectRatio) {
-    emit(state.copyWith(videoAspectRatio: videoAspectRatio));
-    _saveStateAsMergeSettings();
+    if (state case final SettingsBottomSheetLoaded loadedState) {
+      emit(loadedState.copyWith(videoAspectRatio: videoAspectRatio));
+      _saveStateAsMergeSettings();
+    }
   }
 }
--- lib/src/features/merge/presentation/cubits/settings_bottom_sheet_cubit/settings_bottom_sheet_state.dart
+++ lib/src/features/merge/presentation/cubits/settings_bottom_sheet_cubit/settings_bottom_sheet_state.dart
@@ -1,8 +1,26 @@
 import 'package:equatable/equatable.dart';
 import 'package:vmerge/src/features/merge/merge.dart';
 
-final class SettingsBottomSheetState extends Equatable {
-  const SettingsBottomSheetState({
+sealed class SettingsBottomSheetState extends Equatable {
+  const SettingsBottomSheetState();
+}
+
+final class SettingsBottomSheetInitial extends SettingsBottomSheetState {
+  const SettingsBottomSheetInitial();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class SettingsBottomSheetLoading extends SettingsBottomSheetState {
+  const SettingsBottomSheetLoading();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class SettingsBottomSheetLoaded extends SettingsBottomSheetState {
+  const SettingsBottomSheetLoaded({
     required this.isSoundOn,
     required this.playbackSpeed,
     required this.videoResolution,
@@ -14,13 +32,13 @@ final class SettingsBottomSheetState extends Equatable {
   final VideoResolution videoResolution;
   final VideoAspectRatio videoAspectRatio;
 
-  SettingsBottomSheetState copyWith({
+  SettingsBottomSheetLoaded copyWith({
     bool? isSoundOn,
     PlaybackSpeed? playbackSpeed,
     VideoResolution? videoResolution,
     VideoAspectRatio? videoAspectRatio,
   }) {
-    return SettingsBottomSheetState(
+    return SettingsBottomSheetLoaded(
       isSoundOn: isSoundOn ?? this.isSoundOn,
       playbackSpeed: playbackSpeed ?? this.playbackSpeed,
       videoResolution: videoResolution ?? this.videoResolution,
@@ -35,4 +53,27 @@ final class SettingsBottomSheetState extends Equatable {
         videoResolution,
         videoAspectRatio,
       ];
+}
+
+final class SettingsBottomSheetError extends SettingsBottomSheetState {
+  const SettingsBottomSheetError({
+    required this.errorType,
+    required this.error,
+    required this.stackTrace,
+  });
+
+  final SettingsBottomSheetErrorType errorType;
+  final Object error;
+  final StackTrace stackTrace;
+
+  @override
+  List<Object?> get props => [
+        errorType,
+        error,
+        stackTrace,
+      ];
+}
+
+enum SettingsBottomSheetErrorType {
+  saveSettingsException,
 }
